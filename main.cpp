264e702f87b99aac999c9c2c064abc2863920b05
Scott Wright <77592598+RivianSaints@users.noreply.github.com>
2022-12-06 14:43:01 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include "mbed.h"
+#include "USBSerial.h"
+//test program to recieve serial communication
+//this will recieve comm from teraterm, but should work for windows too
+// arduino-upload.bat COM9 BUILD/ARDUINO_NANO33BLE_SENSE/ARMC6\JLawrence_FinalProject_Comm.bin
+// main() runs in its own thread in the OS
+
+USBSerial ConnectionSerial(true);
+char *StoredMessage[30];
+
+
+int main(){  
+    
+    char spoon[] = "I am focused, Iron Focus\n\r"; // echo back
+    ConnectionSerial.write(spoon, sizeof(spoon));
+    char buff[1];
+    while (true) {
+       for(int i=0; i<31; i++){
+            ConnectionSerial.read(buff, sizeof(buff));
+            if(buff == ";"){
+                break;
+            }
+            StoredMessage[i] = buff;
+            //ConnectionSerial.printf("%s\n\r", buff);
+            ConnectionSerial.write(buff, sizeof(buff));
+            thread_sleep_for(10);
+        }
+        ConnectionSerial.printf("\n\rCompleted Message: ");
+        for(int i1= 0; i1<31; i1++){
+            ConnectionSerial.printf("%s",StoredMessage[i1]);
+        }
+        thread_sleep_for(100);
+    }
+}
+
